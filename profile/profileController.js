aa34b9c196bb1e48681641e1fffb2a39ab2c17e0
thienbao92 <thienbao.vudao@yahoo.com>
2015-05-22 17:17:03 +0300

    profile area'

--- profile/profileController.js
+++ profile/profileController.js
@@ -18,4 +18,16 @@ angular.module('profile.Controller', [])
       })
     } //end function addProfile
 
+  $scope.changePassword = function () {
+    $scope.auth.$changePassword({
+      email: $scope.email,
+      oldPassword: $scope.oldPassword,
+      newPassword: $scope.newPassword
+    }).then(function () {
+      console.log("Password changed successfully!");
+    }).catch(function (error) {
+      console.error("Error: ", error);
+    });
+  };
+
 })

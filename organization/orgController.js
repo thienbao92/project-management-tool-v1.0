4c10e334e7f1693fd495b835f04645ff94088f8b
thienbao92 <thienbao.vudao@yahoo.com>
2015-05-26 16:14:19 +0300

    initial setup nested organization and project

--- organization/orgController.js
+++ organization/orgController.js
@@ -8,8 +8,26 @@ angular.module('organization.Controller', [])
 
 
 
-.controller('orgCtrl', function ($scope, firebaseOrg) {
+.controller('orgCtrl', function ($scope, firebaseOrg, firebaseProject) {
 
-    console.log(firebaseOrg);
+    $scope.organization = firebaseOrg;
+
+    $scope.addOrg = function () {
+      $scope.organization.$add({
+        name: $scope.orgName,
+        owner: $scope.id
+      })
+    }; //end function addOrg
+
+    //add project area
+
+    $scope.projectData = {};
+    $scope.addProject = function (uid) {
+        $scope.project = firebaseProject(uid);
+        $scope.project.$add({
+          projectName: $scope.projectData.name
+        })
+      } //end function addProject
+
 
   }) //End orgCtrl
--- organization/orgServices.js
+++ organization/orgServices.js
@@ -7,12 +7,24 @@
 angular.module('organization.services', [])
 
 
-.factory('firebaseOrg', function (firebaseUrl) {
-
-    var ref = firebaseUrl + '/organization';
-
-    va
-
-
-    return ref;
+.factory('firebaseOrg', function (firebaseUrl, $firebaseArray) {
+    var ref = new Firebase(firebaseUrl + '/organization');
+    var organization = $firebaseArray(ref);
+    return organization;
   }) // END firebaseOrg
+
+.factory('firebaseProject', function (firebaseUrl, $firebaseArray) {
+    //
+    //    var ref = new Firebase(firebaseUrl + '/project');
+    //    var project = $firebaseArray(ref);
+    //    return project;
+
+    return function (id) {
+        var ref = new Firebase(firebaseUrl + '/organization/' + id + '/project');
+        var project = $firebaseArray(ref);
+        return project;
+      } //End function
+
+
+
+  }) // END firebaseProject

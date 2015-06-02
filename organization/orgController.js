86038836af8a60029458a5ab4886265426c6d37e
thienbao92 <thienbao.vudao@yahoo.com>
2015-06-02 15:25:32 +0300

    tạo key array

--- organization/orgController.js
+++ organization/orgController.js
@@ -18,16 +18,31 @@ angular.module('organization.Controller', [])
 })
 
 
-.controller('orgCtrl', function ($scope, firebaseOrg, firebaseProject, firebaseGroupMember, firebaseUrl) {
+.controller('orgCtrl', function ($scope, firebaseOrg, firebaseProject, firebaseGroupMember, firebaseUrl, organization) {
 
     $scope.organization = firebaseOrg;
     $scope.groupMember = firebaseGroupMember;
+    var userId = $scope.id;
+    console.log(userId);
+    var ref = new Firebase(firebaseUrl + '/users/' + userId + '/groupMember');
 
-    var ref = new Firebase(firebaseUrl + '/groupMember/');
+    ref.on("value", function (snapshot) {
+      var groupArray = new Array();
 
-    function addToIndex(id) {
-      ref.child(id).set(true);
-    }
+      snapshot.forEach(function (data) {
+        groupArray.push(data.key());
+        //        console.log();
+
+
+      })
+      console.log(groupArray);
+    })
+
+
+
+
+
+
 
     $scope.addOrg = function () {
       $scope.organization.$add({
@@ -35,7 +50,7 @@ angular.module('organization.Controller', [])
         owner: $scope.id
       }).then(function (data) {
         var orgId = data.key();
-        addToIndex(orgId);
+        organization.addMemberToGroup(orgId, $scope.id);
 
         //        $scope.groupMember.$add({
         //          organizationId: orgId,
--- organization/orgServices.js
+++ organization/orgServices.js
@@ -33,3 +33,18 @@ angular.module('organization.services', [])
 
 
   }) // END firebaseProject
+
+.service('organization', function (firebaseUrl) {
+
+  this.addMemberToGroup = function (groupId, memberId) {
+      var ref = new Firebase(firebaseUrl + '/groupMember/' + groupId + '/member');
+      var userRef = new Firebase(firebaseUrl + '/users/' + memberId + '/groupMember');
+      var groupMemberRef = new Firebase(firebaseUrl + '/groupMember/');
+
+      groupMemberRef.child(groupId).set(true);
+      ref.child(memberId).set(true);
+      userRef.child(groupId).set(true);
+    } //End function
+
+
+})
